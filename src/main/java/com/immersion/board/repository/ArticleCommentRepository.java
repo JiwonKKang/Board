7e689c3e0a1e5c0bec0e78f7bece9e98702bf347
강지원 <kangji149@naver.com>
2023-06-26 08:32:04 +0900

    API 테스트
    Data Rest Test 작성

--- src/main/java/com/immersion/board/repository/ArticleCommentRepository.java
+++ src/main/java/com/immersion/board/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.immersion.board.repository;
 
 import com.immersion.board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
