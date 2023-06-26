ab6d1ff5ab610f6620eafe5228103287b77937f6
강지원 <kangji149@naver.com>
2023-06-26 11:08:06 +0900

    Thymeleaf 페이지 작성

--- src/test/java/com/immersion/board/controller/ArticleControllerTest.java
+++ src/test/java/com/immersion/board/controller/ArticleControllerTest.java
@@ -1,5 +1,6 @@
 package com.immersion.board.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,12 +30,30 @@ class ArticleControllerTest {
         //when&then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
 
     }
 
     @Test
+    @DisplayName("[view][GET] 게시글 (게시판) 페이지 - 정상호출")
+    public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+
+        //given
+
+        //when&then
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
+
+    }
+
+    @Test
+    @Disabled
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상호출")
     public void givenNothing_whenRequestingSearchView_thenReturnsSearchView() throws Exception {
 
@@ -43,12 +62,13 @@ class ArticleControllerTest {
         //when&then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles"));
 
     }
 
     @Test
+    @Disabled
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상호출")
     public void givenNothing_whenRequestingHashtagView_thenReturnsHashtagView() throws Exception {
 
@@ -57,7 +77,7 @@ class ArticleControllerTest {
         //when&then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles"));
 
     }
