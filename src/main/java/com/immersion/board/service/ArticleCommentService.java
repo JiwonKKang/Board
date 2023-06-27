21d941cc6eee861cff8abcfaf2b75348e4ba33f6
강지원 <kangji149@naver.com>
2023-06-27 21:52:05 +0900

    댓글 기능 구현

--- src/main/java/com/immersion/board/service/ArticleCommentService.java
+++ src/main/java/com/immersion/board/service/ArticleCommentService.java
@@ -1,14 +1,21 @@
 package com.immersion.board.service;
 
+import com.immersion.board.domain.Article;
+import com.immersion.board.domain.ArticleComment;
 import com.immersion.board.dto.ArticleCommentDto;
 import com.immersion.board.repository.ArticleCommentRepository;
 import com.immersion.board.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -19,16 +26,32 @@ public class ArticleCommentService {
 
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComments(Long articleId) {
-        return List.of();
+        return articleCommentRepository.findByArticle_Id(articleId)
+                .stream()
+                .map(ArticleCommentDto::from)
+                .toList();
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+        } catch (EntityNotFoundException e) {
+            log.warn("저장하려는 게시글을 찾을 수 없습니다.");
+        }
     }
 
     public void updateArticleComment(ArticleCommentDto dto) {
+        try {
+            ArticleComment comment = articleCommentRepository.getReferenceById(dto.id());
+            if (StringUtils.hasText(dto.content())) { comment.setContent(dto.content()); }
+        } catch (EntityNotFoundException e) {
+            log.warn("수정하려는 댓글을 찾을 수 없습니다.");
+        }
+
     }
 
     public void deleteArticleComment(Long articleCommentId) {
+        articleCommentRepository.deleteById(articleCommentId);
     }
 
 }
